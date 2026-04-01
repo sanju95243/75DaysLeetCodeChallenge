b783a15897b9d58ed666e8706bfc4818de990f05
sanju95243 <sanjna2436@gmail.com>
2026-04-01 17:01:59 +0530

    Time: 0 ms (100%), Space: 17.1 MB (66.33%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <string>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+
+        for(string token : tokens) {
+            
+            if(token == "+" || token == "-" || token == "*" || token == "/") {
+                
+                int b = st.top(); st.pop();
+                int a = st.top(); st.pop();
+
+                if(token == "+") st.push(a + b);
+                else if(token == "-") st.push(a - b);
+                else if(token == "*") st.push(a * b);
+                else st.push(a / b); // integer division
+            }
+            else {
+                st.push(stoi(token)); // string → int
+            }
+        }
+
+        return st.top();
+    }
+};

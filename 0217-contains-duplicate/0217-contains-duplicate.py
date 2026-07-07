3ec2cb57398ff069b28e940751f59d7e8cf32773
sanju95243 <sanjna2436@gmail.com>
2026-07-07 12:59:01 +0530

    Time: 32 ms (18.98%), Space: 26.8 MB (6.57%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,11 +1,15 @@
-class Solution:
+class Solution(object):
     def containsDuplicate(self, nums):
-        s = set()
+       hashmap = {}
 
-        for num in nums:
-            if num in s:
-                return True
-            s.add(num)
+       for i in range(len(nums)):
+        if nums[i] in hashmap:
+            return True
+        hashmap[nums[i]] = True
+            
+       return False    
 
-        return False
+nums = [1,2,3,1]
+obj = Solution()
+print(obj.containsDuplicate(nums))
         

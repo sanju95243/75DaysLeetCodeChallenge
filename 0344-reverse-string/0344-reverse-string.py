f58f6be92b91966062b3875d9baff8e6cd5618c2
Sanjna <sanjna2436@gmail.com>
2026-08-21 00:05:13 +0530

    Time: 3 ms (54.95%), Space: 19.8 MB (76.59%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,16 +1,14 @@
-class Solution:
-    def reverseString(self, s: List[str]):
+class Solution(object):
+    def reverseString(self, s):
         left = 0
-        right = len(s) - 1
-        while left < right:
-            s[left],s[right] = s[right],s[left]
+        right = len(s)-1
+
+        while left<right:
+            s[left], s[right] = s[right], s[left]
             left +=1
-            right -= 1
-
-obj = Solution()
-print(obj.reverseString(["h","e","l","l","o"]))
-
-
+            right -=1
+        return s   
+            
 
 
         
